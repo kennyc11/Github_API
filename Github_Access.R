c1a2c6c5a8eabbfafc2d6a67b82353ef7d7d6285
kennyc11 <43179667+kennyc11@users.noreply.github.com>
2018-11-28 22:06:38 +0000

    Connecting R to Plotly

--- Github_Access.R
+++ Github_Access.R
@@ -18,7 +18,7 @@ github_token <- oauth2.0_token(oauth_endpoints("github"), myapp) #run this line 
 1
 
 # Use API
-gtoken <- config(token = github_token)
+getToken <- config(token = github_token)
 #req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
 
 # Take action on http error
@@ -36,7 +36,7 @@ gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"]
 #The above code was sourced from Michael Galarnyk's blog post
 #https://towardsdatascience.com/accessing-data-from-github-api-using-r-3633fb62cb08
 
-
+#Step 1: Interrogating my own Github and my followers github
 
 #Interrogating my own github "kennyc11"
 myGit  <- fromJSON("https://api.github.com/users/kennyc11")
@@ -45,30 +45,47 @@ myGit$followers #returns the number of people who are following me
 myGit$login #returns my login username
 myGit$public_repos #returns the number of public repositories I have
 
+lcaRepos <- fromJSON("https://api.github.com/repos/ohalloa2/CS3012_LCA/commits")
+lcaRepos$commit$message #The details I included describing each commit to LCA assignment repository
+
 #An interrogation of my repositories in github
 myRepos <- fromJSON("https://api.github.com/users/kennyc11/repos")
-myRepos$language #returns the languages I have used in my repositories
-length(myRepos$language) #returns the count of languages used in my repositories
+languagesUsed <- myRepos$language #returns the languages I have used in my repositories
+aggregate(data.frame(count = languagesUsed), list(value =languagesUsed), length) #Data frame displaying count for each language 
+#df_uniq = unique(languagesUsed)
+#length(df_uniq)
 
-#Returns list of people as a matrix of who I am following on github
+#Looking at the commits in my repository for task 1 Software Engineering
+myReposCommits <- fromJSON("https://api.github.com/repos/kennyc11/Software-Engineering/commits")
+myReposCommits$commit$message #Shows the messages for my commits in this repository
+
+
+
+#Returns list of people of who I am following on github
 myUsers <- fromJSON("https://api.github.com/users/kennyc11/following")
 myUsers$login #returns the username logins of my followers on github
 mikeBostock <- myUsers[1,] #gets the first entry in the row from my followers usernames 
 mikeBostock$login #returns the login username of Mike Bostock
 
 mikeBostock$organizations_url #gets the url for mike Bostocks organisations in github
-mikeBostock$repos_url
+mikeBostock$repos_url #url for Mike Bostocks repository in Github
 
+#Interrogation of Mike Bostock's github
 mBostockRepos <- fromJSON("https://api.github.com/users/mbostock/repos") 
 mBostockRepos$name #returns the name of Mike Bostocks repostiroies names
 mBostockRepos$private #returns if Mike Bostocks repos are private or not
-mBostockRepos$language #returns the lanaguages of Mike Bostocks repositories
+mBostockLanguages <- mBostockRepos$language 
+mBostockLanguages #returns the lanaguages of Mike Bostocks repositories
+
+#returns the count of the languages used by Mike Bostock in his repositories
+aggregate(data.frame(count = mBostockLanguages), list(value =mBostockLanguages), length) 
+
 
 #Interrogating Mike Bostocks organisations 
 mBostockOrgs <- fromJSON("https://api.github.com/users/mbostock/orgs") 
 mBostockOrgs$login #returns a list of Mike Bostocks companies
-mBostockOrgs$members_url
-mBostockOrgs$repos_url
+mBostockOrgs$members_url #returns the url for the members on github of the organisations that Mike Bostocks is a member of
+mBostockOrgs$repos_url #returns the url for the repositories that Mike Bostock is a member of
 
 #Interrogation of members of Mike Bostock Organisation d3
 mBostockOrgsd3 <- fromJSON("https://api.github.com/orgs/d3/members")
@@ -77,9 +94,25 @@ mBostockOrgsd3$login #returns the username of d3 members
 #Interrogation of d3 repos
 d3Repos <- fromJSON("https://api.github.com/orgs/d3/repos")
 d3Repos$name #returns the name of d3's repositories
-d3Repos$
+
+#Interrogating the Github API and returning relevant information as data frames
+
+myDataJSon = toJSON(myGit, pretty = TRUE)
+myDataJSon
 
 
 
+#Step two, visulisation interrogation of API
+
+
+#Installing plotly for better visualisation of graphs
+install.packages("plotly")
+require(devtools)
+library(plotly)
+
+#Linking R to plotly. This allows me to create online interactive graphs through interrogation github API
+#building visulation of the data available and uploading to plotly using graphs based on d3s library.
+Sys.setenv("plotly_username"="kennyc11")
+Sys.setenv("plotly_api_key"="zFLJG9Kb2on2xdumwWg1")
 
 
